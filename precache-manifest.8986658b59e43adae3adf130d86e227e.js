baee3a22e454fa25d2350d49a7ac640dd67edf7a
Marcos Ojeda <subliminal@gmail.com>
2019-11-08 06:32:30 -0800

    Updates

---
+++ precache-manifest.8986658b59e43adae3adf130d86e227e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e7f110a054238afe46f19aa2c2d87e",
+    "url": "/binario/index.html"
+  },
+  {
+    "revision": "03de30823ce53036f361",
+    "url": "/binario/static/css/main.ecdf2cfa.chunk.css"
+  },
+  {
+    "revision": "7688cdb44cc59cfdcdf0",
+    "url": "/binario/static/js/2.02a7b6b1.chunk.js"
+  },
+  {
+    "revision": "03de30823ce53036f361",
+    "url": "/binario/static/js/main.f14157f9.chunk.js"
+  },
+  {
+    "revision": "a611388faae0bd98be07",
+    "url": "/binario/static/js/runtime-main.5311f199.js"
+  }
+]);
